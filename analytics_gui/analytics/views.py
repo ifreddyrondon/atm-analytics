d376f1c1abfa2537676809d5e4d8e1de9e8eddeb
Freddy Rondon <spantons@gmail.com>
2016-01-22 12:43:23 -0430

    fix: when do a double save inside case view the journal files was deleted

--- analytics_gui/analytics/views.py
+++ analytics_gui/analytics/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from analytics_gui.analytics.forms import CreateCaseForm, CreateAtmFormSet
-from analytics_gui.analytics.models import Case, AtmCase, AtmJournal
+from analytics_gui.analytics.models import Case, AtmJournal
 
 
 def dashboard(request):
@@ -30,12 +30,15 @@ def create(request):
                 for index, atm in enumerate(atms):
                     journal_virtual_key = 'atms-{}-journal_virtual'.format(index)
                     journal_files = request.FILES.getlist(journal_virtual_key)
-                    if journal_files > 0:
+                    if len(journal_files) > 0:
                         AtmJournal.objects.filter(atm=atm).delete()
                         for journal_file in request.FILES.getlist(journal_virtual_key):
                             AtmJournal.objects.create(atm=atm, file=journal_file)
 
-                return HttpResponseRedirect(reverse("analytics:dashboard"))
+                if 'save' in request.POST:
+                    return HttpResponseRedirect(reverse("analytics:dashboard"))
+                elif 'analyze' in request.POST:
+                    return HttpResponseRedirect(reverse("analytics:analyze", args=[case.id]))
 
     return render(request, 'analytics/create.html', {
         'form': form,
@@ -56,18 +59,31 @@ def view_case(request, case_id):
             case.save()
             atm_form_set.save()
             for index, atm in enumerate(case.atms.all()):
-                print(index)
                 journal_virtual_key = 'atms-{}-journal_virtual'.format(index)
                 journal_files = request.FILES.getlist(journal_virtual_key)
-                if journal_files > 0:
+                if len(journal_files) > 0:
                     AtmJournal.objects.filter(atm=atm).delete()
                     for journal_file in request.FILES.getlist(journal_virtual_key):
                         AtmJournal.objects.create(atm=atm, file=journal_file)
+
+            if 'analyze' in request.POST:
+                return HttpResponseRedirect(reverse("analytics:analyze", args=[case.id]))
 
     return render(request, 'analytics/create.html', {
         'form': form,
         'atm_form_set': atm_form_set,
         'create': False
+    })
+
+
+def analyze_case(request, case_id):
+    case = get_object_or_404(Case, id=case_id)
+    for atm in case.atms.all():
+        journal_files = atm.journals.all().values_list('file', flat=True)
+        print(journal_files)
+
+    return render(request, 'analytics/results.html', {
+        'case': case
     })
 
 
