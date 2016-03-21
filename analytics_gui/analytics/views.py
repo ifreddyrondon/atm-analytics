4ddb5ffc8e5c082ad656f5edf45e64b0a412e726
Freddy Rondon <spantons@gmail.com>
2016-03-21 13:44:35 -0430

    feat(DJAN-30): update style and format of “Amount of cash events involving the analyzed ATM addresses” with address, count of events by atm and replacement schedule

--- analytics_gui/analytics/views.py
+++ analytics_gui/analytics/views.py
@@ -138,7 +138,7 @@ def analyze_case(request, case_id):
             "min_date": None,
             "max_date": None,
             "count": 0,
-            "atms_index_count": [],
+            "atms_count": [],
             "close_events_count": 0,
         }
     }
@@ -185,7 +185,11 @@ def analyze_case(request, case_id):
 
         # reposition events
         atm_reposition_events = AtmRepositionEvent.objects.filter(bank=case.bank, location=atm.atm_location.first())
-        meta["reposition"]["atms_index_count"].append(len(atm_reposition_events))
+        meta["reposition"]["atms_count"].append({
+            'address': atm.atm_location.first().address,
+            'count': len(atm_reposition_events),
+            'date': atm_reposition_events[0].reposition_date.strftime("%H:%M")
+        })
         meta["reposition"]["count"] += len(atm_reposition_events)
         for event in atm_reposition_events:
             # get the min and max dates for filter
